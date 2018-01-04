4306cafa90405e2236d9de42eb3c1facc34b3d77
zhiwei <tzw.acnt@gmail.com>
2018-01-04 20:35:13 +0800

    bugfix

--- app/main/forms.py
+++ app/main/forms.py
@@ -8,6 +8,6 @@ from wtforms.validators import InputRequired
 class PostForm(FlaskForm):
     title = StringField(validators=[InputRequired()])
     #tags = StringField(validators=[InputRequired()])
-    #category = StringField(validators=[InputRequired()])
+    category = StringField(validators=[InputRequired()])
     body = TextAreaField(validators=[InputRequired()])
     submit = SubmitField("Submit")
